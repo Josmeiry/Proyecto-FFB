9b6594b4ce746fb4892d90c8d3bdd4d267aafc11
Josmeiry <josmeriymy26@gmail.com>
2026-04-30 09:41:44 -0400

    corrigiendo errores vercel

--- src/main.js
+++ src/main.js
@@ -3,11 +3,24 @@ import { createApp } from 'vue'
 
 import './assets/main.css'
 import './style.css'
-import './javas.js'
+// import './javas.js'
 import router from './router'
 import App from './App.vue'
 
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
 
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
 
 createApp(App).use(router).mount('#app')
 
